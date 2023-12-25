dd6af1c7b132883ea7d08099242cd9dd05b91f85
Aymone SOH <aymonecynthiasoh@gmail.com>
2023-12-25 17:29:57 +0100

    premier commit

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Header from './components/Header'
-import ClientForm from './components/ClientForm'
-import FreelanceForm from './components/FreelanceForm'
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,8 +13,6 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey" element={<Survey />} />
-          <Route path="/survey/client" element={<ClientForm />} />
-          <Route path="/survey/freelance" element={<FreelanceForm />} />
       </Routes>
     </Router>
   </React.StrictMode>,
